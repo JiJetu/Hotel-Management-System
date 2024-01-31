2639cf16add7ca73017e7afe8315e597f993c673
JiJetu <jijetu2@gmail.com>
2024-01-31 20:30:39 +0600

    updated manage section and add the fuctionality of the manage button

---
+++ server/index.js
@@ -0,0 +1,173 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json())
+
+
+// console.log(process.env.DB_USER);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dscp8or.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const treeHouseDB = client.db("TreeHouse")
+    const categories = treeHouseDB.collection("categories")
+    const addProduct = treeHouseDB.collection("addProduct")
+    const bookings = treeHouseDB.collection("bookings")
+
+
+    app.get('/categories', async (req, res) => {
+      const categoriesList = req.body
+      const result = await categories.find(categoriesList).toArray()
+      res.send(result)
+    })
+    app.get('/categories/:path', async (req, res) => {
+      const path = req.params.path
+      const query = { path: path };
+      const options = {
+        projection: { _id: 0, image: 1, categories: 1 },
+      };
+      const categoryPath = await categories.findOne(query, options)
+      res.send(categoryPath)
+    })
+
+
+    // Added Product
+    app.get('/addProducts', async (req, res) => {
+      // const products = req.body;
+      const result = await addProduct.find().toArray();
+      res.send(result)
+    })
+
+    app.get('/addProducts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await addProduct.findOne(query)
+      res.send(result)
+    })
+
+    app.post('/addProducts', async (req, res) => {
+      const product = req.body;
+      const result = await addProduct.insertOne(product)
+      res.send(result)
+    })
+
+    app.put('/addProducts/:id', async (req, res) => {
+      const product = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+
+      const updateProduct = {
+        $set: {
+          productName: product.productName,
+          selectedProduct: product.selectedProduct,
+          image: product.image,
+          price: product.price,
+          productDescription: product.productDescription,
+          productFeatures: product.productFeatures
+        },
+      };
+      const result = await addProduct.updateOne(filter, updateProduct, options);
+
+      res.send(result);
+    })
+
+    app.delete('/addProducts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await addProduct.deleteOne(query);
+      res.send(result)
+    })
+
+
+
+    // find product
+    app.get('/category/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      // const options = {
+      //   projection: { _id: 0 },
+      // };
+      const product = await addProduct.findOne(query)
+      res.send(product)
+    })
+
+    // bookings
+    app.get('/bookings', async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email }
+      }
+      const result = await bookings.find(query).toArray();
+      res.send(result)
+    })
+
+
+    app.post('/bookings', async (req, res) => {
+      const bookingsItem = req.body;
+      // const {date} = bookingsItem.date;
+      const isExists = await bookings.findOne(bookingsItem);
+      if(isExists){
+        return res.json("already exists")
+      }
+      const result = await bookings.insertOne(bookingsItem);
+      res.send(result)
+    })
+
+    app.patch('/bookings/:id', async (req, res) => {
+      const data = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateData = {
+        $set: {
+          status: data.status
+        },
+      };
+      const result = await bookings.updateOne(filter, updateData)
+      res.send(result)
+    })
+
+    app.delete('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookings.deleteOne(query);
+      res.send(result);
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send("server is running");
+})
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+})
